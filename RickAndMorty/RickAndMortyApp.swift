e6db1decd0080277b000ddcb43edacac76a3d0cd
Thomas Garayua <tgarayua@github.com>
2023-05-13 13:40:23 -0400

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Thomas Garayua on 5/13/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
