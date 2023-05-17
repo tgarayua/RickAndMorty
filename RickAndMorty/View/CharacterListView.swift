db23c1e2d6be726e4d874853e0fa1f911a70f571
Thomas Garayua <tgarayua@github.com>
2023-05-17 01:13:47 -0400

    added spacing between the status indicator and the status

--- RickAndMorty/View/CharacterListView.swift
+++ RickAndMorty/View/CharacterListView.swift
@@ -18,11 +18,11 @@ struct CharacterListView: View {
                     Text(character.name)
                         .font(.title)
                     if (character.status == "Alive") {
-                        (Text(Image(systemName: "circle.fill")).foregroundColor(.green) + Text(character.status) + Text(" - ") + Text(character.species))
+                        (Text(Image(systemName: "circle.fill")).foregroundColor(.green) + Text(" ") + Text(character.status) + Text(" - ") + Text(character.species))
                     } else if (character.status == "Dead") {
-                        (Text(Image(systemName: "circle.fill")).foregroundColor(.red) + Text(character.status) + Text(" - ") + Text(character.species))
+                        (Text(Image(systemName: "circle.fill")).foregroundColor(.red) + Text(" ") + Text(character.status) + Text(" - ") + Text(character.species))
                     } else {
-                        (Text(Image(systemName: "circle.fill")).foregroundColor(.gray) + Text(character.status) + Text(" - ") + Text(character.species))
+                        (Text(Image(systemName: "circle")).foregroundColor(.gray) + Text(" ") + Text(character.status) + Text(" - ") + Text(character.species))
                     }
                     Text("Last Known Location: ").foregroundColor(.secondary)
                     Text(character.location.name)
